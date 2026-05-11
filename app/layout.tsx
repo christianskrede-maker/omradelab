aa2225e01e48e6e5e1b0d5d633e4676803579be3
christianskrede-maker <christianskrede@gmail.com>
2026-05-11 14:25:40 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,1 +1,16 @@
+export const metadata = {
+  title: "OmrådeLab",
+  description: "Dialog mellom byggeprosjekter og nærmiljø"
+};
 
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="no">
+      <body>{children}</body>
+    </html>
+  );
+}
