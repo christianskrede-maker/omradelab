e5993c0d9aa6c2e5552e10b32f0e02e05b5892e2
christianskrede-maker <christianskrede@gmail.com>
2026-05-11 19:19:31 +0200

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,1 +1,19 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
 
+export function middleware(request: NextRequest) {
+  const host = request.headers.get("host") || "";
+  const pathname = request.nextUrl.pathname;
+
+  if (host === "hotvet.omradelab.com" && pathname === "/") {
+    const url = request.nextUrl.clone();
+    url.pathname = "/p/betonmast/buskerud-vestfold/prosjekt/hotvet";
+    return NextResponse.rewrite(url);
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/((?!api|_next|favicon.ico).*)"],
+};
