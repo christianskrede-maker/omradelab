68ed8341d248f4cbc29941bef904cd7181513f86
christianskrede-maker <christianskrede@gmail.com>
2026-05-11 20:28:05 +0200

    Update route.ts

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -34,7 +34,6 @@ export async function POST(request: Request) {
 
       subject: `Ny henvendelse fra Hotvet - ${navn}`,
 
-      replyTo: epost || undefined,
 
       text: `
 Ny henvendelse fra Hotvet
