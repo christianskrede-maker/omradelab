31f4ac65ea3abc0c68230c507539a69facbe3540
christianskrede-maker <christianskrede@gmail.com>
2026-05-11 20:07:23 +0200

    Update route.ts

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -28,7 +28,7 @@ export async function POST(request: Request) {
     }
 
     const { error } = await resend.emails.send({
-      from: "onboarding@resend.dev",
+      from: "OmrådeLab <kontakt@omradelab.com>",
 
       to: ["christianskrede@gmail.com"],
 
